f4d6344bf16612609ae1bcfc99e9d2ccd2c23f58
KorayhanAvcu <korayhanavcu@gmail.com>
2026-09-16 14:11:19 +0300

    devam edilecek

--- Travel.Web/ViewComponents/Tour-Breadcrumb/_TourBreadcrumbViewComponent.cs
+++ Travel.Web/ViewComponents/Tour-Breadcrumb/_TourBreadcrumbViewComponent.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Travel.Web.DTOs.TourDtos;
 
 namespace Travel.Web.ViewComponents.Tour_Breadcrumb
 {
     public class _TourBreadcrumbViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(TourDetailDto model)
         {
-            return View();
-
+            return View(model);
         }
     }
 }
